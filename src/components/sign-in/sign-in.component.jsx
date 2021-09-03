0464781894a7019b79c7eddce8be15535a85742f
Ray <qc@pop-os.localdomain>
2021-09-03 18:32:28 +0530

    Custom Form Input and Custom Button Component Added

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,61 @@
+import React,{Component} from 'react';
+import './sign-in.style.scss'
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component';
+
+
+
+class SignInComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit =(e)=>{
+         e.preventDefault();
+
+         this.setState({email:'', password:''});
+    }
+
+    handleChange=(e)=>{
+      const {value ,name} = e.target;
+
+      this.setState({[name] : value})
+    }
+
+
+render() {
+    return (
+        <div className='sign-in'>
+            <h2 className='title'>I already have an account</h2>
+            <span>Sign in with your email and Password</span>
+
+            <form onSubmit={this.handleSubmit}>
+                <FormInput name='email' type='email' value={this.state.email}
+                label='email'
+                handleChange={this.handleChange}
+                
+                required/>
+                 
+                 <FormInput name='password' type='password' value={this.state.password}
+                 label='password'
+                handleChange={this.handleChange}
+                 
+                 required/>
+                
+
+                 <CustomButton type='submit' > Sign IN </CustomButton>
+            </form>
+
+        </div>
+    )
+}
+
+}
+
+
+export default SignInComponent
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+import SignInComponent from '../../components/sign-in/sign-in.component';
+
+
+
+const SignInAndSignUpPage = ()=>(
+
+    <div className="sign-in-and-sign-up">
+        <SignInComponent/>
+    </div>
+)
+
+export default SignInAndSignUpPage;
